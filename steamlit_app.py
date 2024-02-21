194ea181b9c629f10ae10dae1fc5c940d5242663
buttons87 <160712398+buttons87@users.noreply.github.com>
2024-02-21 18:50:12 +1030

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -5,3 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🍲 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥤 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍳 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avo & Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
