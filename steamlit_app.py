2bb5406887c21bfaf5e0145ca3f44948d5a072ca
buttons87 <160712398+buttons87@users.noreply.github.com>
2024-03-02 12:20:13 +1030

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -17,6 +17,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"Apple")
 fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalize)
