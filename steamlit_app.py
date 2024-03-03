ff053177d58434742a29ca21bcd3d129f900c7d6
buttons87 <160712398+buttons87@users.noreply.github.com>
2024-03-03 21:41:04 +1030

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -20,11 +20,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"Apple")
-fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalize)
-streamlit.stop()
-
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else
+        fruityvice_respose = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+        fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalize)
+except URLError as e:
+    streamlit.error()
+streamlit.stop()    
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
